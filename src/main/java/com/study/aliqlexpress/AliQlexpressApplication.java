c934368a9f061bbf641bbf8ef3d12720ae8733d4
luohx <Lhongxiao@lcchain.com>
2023-08-17 15:49:12 +0800

    创建ali-qlexpress项目

---
+++ src/main/java/com/study/aliqlexpress/AliQlexpressApplication.java
@@ -0,0 +1,16 @@
+package com.study.aliqlexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luohongxiao
+ */
+@SpringBootApplication
+public class AliQlexpressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliQlexpressApplication.class, args);
+    }
+
+}
